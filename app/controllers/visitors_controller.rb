0c2c958d367cbf76e9099e808db77f45767b9de8
Ryan Whitnah <ryan.whitnah@nccgroup.com>
2015-09-14 14:18:33 -0600

    Adds basic querying to main page

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,8 +1,22 @@
 class VisitorsController < ApplicationController
   def index
-    @document = Document.find(1)
-    @document.tag_document
+    @documents = []
+    @query = ''
 
-    @keywords = @document.keywords
+    if params[:query]
+      @query = params[:query]
+      search_service = SearchService.new(@query)
+      results = search_service.search_all
+      results.each do |result|
+        @documents << Document.find(result[:document_id])
+        @documents.compact!
+        @documents.uniq!
+      end
+    end
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @documents }
+    end
   end
 end
